a46bd26298c545adcf0ab7bda68585f272463ac2
Stuart P. Bentley <stuart@testtrack4.com>
2014-08-09 14:54:40 -0700

    install.sh: Only set up plushu user when installing as root
    Now that we have a few things that happen during install that a
    non-root user would need, we should make that possible to do.

--- install.sh
+++ install.sh
@@ -8,11 +8,6 @@ set -eo pipefail
 # Directory to install plushu script to, unset to disable
 BIN_DIR="${BIN_DIR-/usr/local/bin}"
 
-# Link the plushu script into the bin dir
-if [ -n "$BIN_DIR" ]; then
-  ln -sf "$PLUSHU_SCRIPT" "$BIN_DIR"
-fi
-
 # If the root is a git clone, set it to ignore new files
 if [ -d "$PLUSHU_ROOT/.git/info" ]; then
   printf '*\n' > "$PLUSHU_ROOT/.git/info/exclude"
@@ -33,17 +28,25 @@ if [ ! -d "$PLUSHU_ROOT/plugins/help" ]; then
   plushu plugins:install help-plugin
 fi
 
-# Create the plushu user if they do not exist
-if ! id -u plushu >/dev/null 2>&1; then
-  useradd -Md "$PLUSHU_ROOT" -s "$PLUSHU_SCRIPT" plushu
+# If root is performing the installation
+if [[ $EUID == 0 ]]; then
+  # Create the plushu user if they do not exist
+  if ! id -u plushu >/dev/null 2>&1; then
+    useradd -Md "$PLUSHU_ROOT" -s "$PLUSHU_SCRIPT" plushu
+  fi
+
+  # Initialize the ssh settings
+  mkdir -p "$PLUSHU_ROOT/.ssh"
+  touch "$PLUSHU_ROOT/.ssh/authorized_keys"
+  chmod 0700 "$PLUSHU_ROOT/.ssh"
+  chmod 0600 "$PLUSHU_ROOT/.ssh/authorized_keys"
+
+  # Set appropriate ownership and permissions
+  chown -R plushu "$PLUSHU_ROOT"
+  chmod 0711 "$PLUSHU_ROOT"
+
+  # Link the plushu script into the bin dir
+  if [ -n "$BIN_DIR" ]; then
+    ln -sf "$PLUSHU_SCRIPT" "$BIN_DIR"
+  fi
 fi
-
-# Initialize the ssh settings
-mkdir -p "$PLUSHU_ROOT/.ssh"
-touch "$PLUSHU_ROOT/.ssh/authorized_keys"
-chmod 0700 "$PLUSHU_ROOT/.ssh"
-chmod 0600 "$PLUSHU_ROOT/.ssh/authorized_keys"
-
-# Set appropriate ownership and permissions
-chown -R plushu "$PLUSHU_ROOT"
-chmod 0711 "$PLUSHU_ROOT"
