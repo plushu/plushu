98d8e8172b08605282cd61aa449d705dfb213d2d
Stuart P. Bentley <stuart@testtrack4.com>
2014-08-09 14:11:24 -0700

    install.sh: Quote strings, be a little better with permissions

--- install.sh
+++ install.sh
@@ -2,46 +2,48 @@
 set -eo pipefail
 
 # Setup parameters
-: ${PLUSHU_ROOT:=$(cd "$(dirname "$0")" && pwd)}
-: ${PLUSHU_SCRIPT:=$PLUSHU_ROOT/bin/plushu}
+: ${PLUSHU_ROOT:="$(cd "$(dirname "$0")" && pwd)"}
+: ${PLUSHU_SCRIPT:="$PLUSHU_ROOT/bin/plushu"}
 
 # Directory to install plushu script to, unset to disable
-BIN_DIR=${BIN_DIR-/usr/local/bin}
+BIN_DIR="${BIN_DIR-/usr/local/bin}"
 
 # Link the plushu script into the bin dir
-if [ -n $BIN_DIR ]; then
-  ln -sf $PLUSHU_SCRIPT $BIN_DIR
+if [ -n "$BIN_DIR" ]; then
+  ln -sf "$PLUSHU_SCRIPT" "$BIN_DIR"
 fi
 
 # If the root is a git clone, set it to ignore new files
-if [ -d $PLUSHU_ROOT/.git/info ]; then
-  printf '*\n' > $PLUSHU_ROOT/.git/info/exclude
+if [ -d "$PLUSHU_ROOT/.git/info" ]; then
+  printf '*\n' > "$PLUSHU_ROOT/.git/info/exclude"
 fi
 
 # Create an initial .plushurc
-cat >$PLUSHU_ROOT/.plushurc <<"EOF"
+cat >"$PLUSHU_ROOT/.plushurc" <<"EOF"
 PATH=$HOME/bin:$PATH
 EOF
 
 # Install the core plugins
-mkdir -p $PLUSHU_ROOT/plugins
-if [ ! -d $PLUSHU_ROOT/plugins/plugins ]; then
+mkdir -p "$PLUSHU_ROOT/plugins"
+if [ ! -d "$PLUSHU_ROOT/plugins/plugins" ]; then
   git clone https://github.com/plushu/plushu-plugins-plugin \
-    $PLUSHU_ROOT/plugins/plugins
+    "$PLUSHU_ROOT/plugins/plugins"
 fi
-if [ ! -d $PLUSHU_ROOT/plugins/help ]; then
+if [ ! -d "$PLUSHU_ROOT/plugins/help" ]; then
   plushu plugins:install help-plugin
 fi
 
 # Create the plushu user if they do not exist
 if ! id -u plushu >/dev/null 2>&1; then
-  useradd -Md $PLUSHU_ROOT -s $PLUSHU_SCRIPT plushu
+  useradd -Md "$PLUSHU_ROOT" -s "$PLUSHU_SCRIPT" plushu
 fi
 
 # Initialize the ssh settings
-mkdir -p $PLUSHU_ROOT/.ssh
-touch $PLUSHU_ROOT/.ssh/authorized_keys
-chmod g-w $PLUSHU_ROOT $PLUSHU_ROOT/.ssh $PLUSHU_ROOT/.ssh/authorized_keys
+mkdir -p "$PLUSHU_ROOT/.ssh"
+touch "$PLUSHU_ROOT/.ssh/authorized_keys"
+chmod 0700 "$PLUSHU_ROOT/.ssh"
+chmod 0600 "$PLUSHU_ROOT/.ssh/authorized_keys"
 
-# Set appropriate ownership
-chown -R plushu $PLUSHU_ROOT
+# Set appropriate ownership and permissions
+chown -R plushu "$PLUSHU_ROOT"
+chmod 0711 "$PLUSHU_ROOT"
